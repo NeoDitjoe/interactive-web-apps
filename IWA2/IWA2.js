fbb810d318a7e35fc0052a909be1a38a3cff5cc7
NeoDitjoe <ditjoeneo03@gmail.com>
2023-03-29 14:02:16 +0200

    Debugged IWA3

--- IWA2/IWA2.js
+++ IWA2/IWA2.js
@@ -1,4 +1,4 @@
-/* This is the firstname of the user */
+// This is the firstname of the user 
 
 const user = 'John';
 
@@ -30,7 +30,7 @@ console.log(date)
 
 console.warn("Security scan starting")
 
-/* It is important to let user know when they can close the page  */
+// It is important to let user know when they can close the page  
 
 console.info('Please wait for scan to complete before closing the browser.')
 
---
+++ IWA3/IWA3.js
@@ -0,0 +1,8 @@
+// scripts.js
+import { company, year} from './IWA3,1.js'
+
+
+
+const message = '© ' + company + ' (' + year + ')'
+document.querySelector('footer').innerText = message
+
