60199c36c3da85cda8377b1dbf64e3630aab4600
NeoDitjoe <ditjoeneo03@gmail.com>
2023-03-30 11:18:52 +0200

    completed IWA4 also adde IWA3, challenge1 into its own folder

---
+++ IWA4/IWA4.js
@@ -0,0 +1,55 @@
+  let date= 2050;
+  let status="student";
+  let count = 0
+  
+  
+  if (date) {
+    console.log("January", 'New Year’s Day');
+    console.log("March", 'Human Rights Day');
+    date = 'April';
+    console.log(date, 'Family Day');
+    console.log(date, 'Freedom Day');
+    count = count + 4
+    console.log(count)
+    
+  }
+  console.log('')
+  
+      if (status) {
+      console.log(status)
+      console.log('June', 'Youth Day')
+      count = count + 1
+      console.log(count)
+    }
+    
+    console.log('');
+
+    console.log('August', 'Women’s Day');
+    console.log('September', 'Heritage Day');
+    date = 'December' 
+    console.log(date, 'Day of Reconciliation');
+    count = count + 3;
+    console.log(count);
+
+    console.log("")
+    status = "parent";
+  
+    if (status) {
+      console.log(status)
+      console.log(date, 'Christmas Day')
+      count = count + 0
+      console.log(count)
+    } 
+   
+    console.log(date,'Day of Goodwill')
+    count = count + 1
+  
+    console.log("")
+    date = 2050
+  
+  console.log('Your status is:', status)
+  console.log('The year is:', date)
+  console.log('The total holidays is:', count) 
+
+
+  
