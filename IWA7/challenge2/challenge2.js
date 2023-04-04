9cd722d2b7cf88e40074b195375e5d983cbe78eb
NeoDitjoe <ditjoeneo03@gmail.com>
2023-04-04 19:07:12 +0200

    added a pick random function

---
+++ IWA7/challenge2/challenge2.js
@@ -0,0 +1,20 @@
+const nickname= "Timmy";
+const firstname = "Timothy";
+                                                    
+
+
+function Random() {
+    let random = Math.ceil(Math.random()*2);
+    return random;
+  }
+
+let number = Random();
+/* console.log(number) */
+
+if (number == 1){
+    console.log('Good Morning',nickname)
+ }
+
+ else{
+    console.log('Good Morning',firstname)
+ }
