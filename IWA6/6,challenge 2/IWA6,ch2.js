47f07f8cc4aa6f1ad968b0d648fbc8ee21d911f4
NeoDitjoe <ditjoeneo03@gmail.com>
2023-04-04 14:44:35 +0200

    edited 6 ch2

--- IWA6/6,challenge 2/IWA6,ch2.js
+++ IWA6/6,challenge 2/IWA6,ch2.js
@@ -9,8 +9,8 @@ const minuteOfDay = 00;
 
 
 if ( minuteOfDay == 00 && hourOfDay == 00 ) {
-   let taxAsDecimal = 800 /100* parseInt(tax) 
-   taxAsDecimal = 800 - taxAsDecimal 
+   let taxAsDecimal = salary /100* parseInt(tax) 
+   taxAsDecimal = salary - taxAsDecimal 
    const cost= rent + transport + food
    const startingAfterTax = taxAsDecimal - cost /* +32.41 */
    console.log(startingAfterTax.toFixed(2))
