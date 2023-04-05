2a523335809be3260544d2b669ebc36f49518637
NeoDitjoe <ditjoeneo03@gmail.com>
2023-04-05 21:15:38 +0200

    completed 8

--- IWA8/IWA8.js
+++ IWA8/IWA8.js
@@ -1,48 +1,70 @@
-console.log("f;lh")
-
 const leoName = 'Leo Musvaire'
 const leoNumber = '2'
 const leoStreet = 'Church St.'
 const leoPostal = '3105'
 const leoBalance = '-10'
 
-/* const sarahName = 'Sarah    '
+const sarahName = 'Sarah    '
 const sarahSurname = 'Kleinhans'
 const sarahBalance = '-4582.21000111'
 const sarahNumber = '13'
 const sarahStreet = 'William Close'
 const sarahPostal = '0310'
-const sarahBalance = '-5' */
 
-// Only change below this line
 
-const leo = {
+
+// Only change below this line 
+
+const leo = {                         //renamed variable names
 	name : leoName,
-	balance: parseInt(leoBalance),
-	age : 24,
+	balance: parseInt(leoBalance),    
+	age : 24,                        // I could a have made this a string and and give it a variable
 
 	address:{
 		number : parseInt(leoNumber),
-		street : leoStreet,
+		street : leoStreet,             //made this an object
 		postal : parseInt(leoPostal)
 	}
-	/* ${"--------------------------------------"} */
+	
 };
+
+
+
+const sarahtrim = sarahName.trim();          //removed empty space 
+let sarahbal = parseFloat(sarahBalance)      //so the number stay a decimal number
+sarahbal = sarahbal.toFixed(2)
+
+
+const leoCredit =  parseFloat(leo.balance)  //combine their balance
+const sarahCredit = parseFloat(sarahbal)
+const credit = leoCredit + sarahCredit
+
+
+
+const sarah = `
+${sarahtrim} ${sarahSurname} age: 62 (Balance: R${sarahbal})
+Address: number: ${sarahNumber}                      
+         street: ${sarahStreet}
+		 postal: ${sarahPostal}
+		
+
+----------------------------------------------
+    Total Balance: R ${credit}
+----------------------------------------------
+`;
+
+
+
 console.log (`
-        ${leo.name +" "+"age:" + " " + leo.age+ ' ('+'Balance:' +' '+ leo.balance.toFixed(2) +')'} `);
-		console.log("Leo Adress" , leo.address)
+${leo.name +" "+"age:" + " " + leo.age+ ' ('+'Balance:' +' '+ leo.balance.toFixed(2) +')'} `);
+		console.log("Adress" , leo.address, `
+		                 
+		${sarah}`)    //space between leo and sarah's detail
+
+		
 
 
-/* const sarah = {
-	name : saraName -*-
-	age : 62
-	access id : 6b279ae5-5657-4240-80e9-23f6b635f7a8 
-	balance : sarahBalance
-	address: {
-		number : sarahNumber
-		stree: : sarahStreet
-		postal-code = sarahPostal
-	} 
 
-console.log() */
+
+
   
