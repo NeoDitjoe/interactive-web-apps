b6a26917a72a16e3b536b45b08aa0fcb99268ccb
NeoDitjoe <ditjoeneo03@gmail.com>
2023-04-17 15:08:19 +0200

    Completed 14 challenge 2

--- IWA14/Challenge2/IWA14Challenge2.js
+++ IWA14/Challenge2/IWA14Challenge2.js
@@ -1,62 +1,21 @@
-/* // script.js
-
-function add(a, b) { 
-  return a + b 
-}
-
-function multiply(a, b) { 
-  return a * b 
-}
-
-function internal() {
-	const added = this.add(this.internal.a, this.internal.b)
-	return this.multiply(added, this.internal.c)
-}
-
-// Not allowed to change below this
-
-const example1 = {
-	internal: {
-		a: 2,
-		b: 4,
-		c: 8,
-	},
-	add,
-	multiply,
-  calculate: internal
-}
-
-const example2 = {
-	internal: {
-		a: 2,
-		b: 2,
-		c: 3,
-	},
-	add,
-	multiply,
-  calculate: internal
-}
-
-console.log(example1.calculate()) // output: 48
-console.log(example2.calculate()) // output: 12
- */
 
 // script.js
 
 function add (a, b ) {
-	 return a + b 
+	 return a + b
 	}
 
-	console.log(add(1, 4))
 
-function multiply (a, b ) { 
-	a - b 
+function multiply (a, b) { 
+	return  a *  b 
 }
 
 function internal() {
-	const added = this.add(this.a, this.b)
-	this.multiply(this.a, this.b)
-	return this
+
+	add = this.add(this.internal.a, this.internal.b)
+	multiply = this.multiply(add, this.internal.c)  
+
+	console.log(multiply)
 }
 
 // Not allowed to change below this
@@ -84,4 +43,4 @@ const example2 = {
 }
 
 example1.calculate()
-example2.calculate()
+example2.calculate() 
