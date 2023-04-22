5304bc258edd46310d6aed67ab47043656eb08d2
NeoDitjoe <ditjoeneo03@gmail.com>
2023-04-22 16:33:51 +0200

    Got the date to appear

--- IWA17/loops.js
+++ IWA17/loops.js
@@ -13,86 +13,119 @@ const MONTHS = [
     'December',
 ]
 
-const getDaysInMonth = (date) => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate()
+const getDaysInMonth = (date) => new Date(date.getFullYear() , date.getMonth() + 1, 0).getDate()
 
 // Only edit below 
+
 
 const createArray = (length) => {
     const result = []
 
-    for (let i = 0; i < createArray.length ; i++) {
-        result
-    }
+    for (let i = 0; i < length ; i++) {
+         return result
+    } 
+
+ 
 }
+    const createData = (data) => {
+        
+     ////////////////////////calculates the days in a month which is 30////////////////////////////////////
+     
+     const current = new Date
+     current.setDate(0)
+     
+     startDay = current.day
+     daysInMonth = getDaysInMonth(current)
+   
+     weeks = createArray(5)
+     days = createArray(7)
+     value = null
+    
 
-const createData = () => {
-    const current = new Date
-    current.setDate(1)
-
-    startDay = current.day
-    daysInMonth = getDaysInMonth(current)
-
-    weeks = createArray(5)
-    days = createArray(7)
-    value = null
 
     for (weekIndex in weeks) {
         value = [{
             week: weekIndex + 1,
             days: []
         }]
+      
+        
+        
 
-        for (dayIndex in days) {
-            value = dayIndex - startDay
-            isValid = day > 0 && day <= daysInMonth
+         for (let dayIndex of days) {  //let
+             value = dayIndex - startDay
+             isValid = days > 0 && days <= daysInMonth  //day  to days
 
-            result[weekIndex].days = [{
-                dayOfWeek: dayIndex + 1,
-                value: isValid && day,
-            }]
-        }
-    }
-}
+             result[weekIndex].days = [{
+                 dayOfWeek: dayIndex + 1,
+                 value: isValid && days,   //day  to days
+             }]         
+         }
+    }  
+    
+    
+ } 
 
-const addCell = (existing, classString, value) => {
-    const result = /* html */ `
-        <td ${classString}>
-            ${value}
-        </td>
+ /////////////////////////////////////////////////////////function//////////////////////////////////////////////
+ const addCell = ( existing, classString, value ) => {
+      const result = /* html */ 
+      `
+         <td ${classString} > 
+             ${value}
+         </td>
 
         ${existing}
-    `
-}
 
-const createHtml = (data) => {
-    let result = ''
+     `  
+     return result
+ } ////////////////////////// <td ${classString} >     
 
-    for (week, days in data) {
-        let inner = ""
-        addCell(inner, 'table__cell table__cell_sidebar', 'Week {week}')
+
+ /////////////////////////////////////////////////////////function//////////////////////////////////////////////
+ const createHtml = (data) => {
+
+     let result = [];
+
+     for (let week2 /* days*/ in data) {    //week, day of data //let
+         let inner =[]
+         addCell(inner, 'table__cell table__cell_sidebar', 'Week {week}')
     
-        for (dayOfWeek, value in days) {
-            classString = table__cell
-						isToday = new Date === value
-            isWeekend = dayOfWeek = 1 && dayOfWeek == 7
-            isAlternate = week / 2
+         for (let  dayOfWeek2/* value*/  of days) {    //let
+             classString = table__cell
+ 			            isToday = new Date === value
+             isWeekend = dayOfWeek2 = 1 && dayOfWeek == 7
+             isAlternate = week / 2
 
-            let classString = 'table__cell'
+             let classString = 'table__cell'
 
-						if (isToday) classString = `${classString} table__cell_today`
-            if (isWeekend) classString === '{classString} table__cell_weekend'
-            if (isAlternate) classString === '{classString} table__cell_alternate'
-            addCell(inner, classString, value)
-        }
+ 			if (isToday) classString = `${classString} table__cell_today`
+             if (isWeekend) classString === '{classString} table__cell_weekend'
+             if (isAlternate) classString === '{classString} table__cell_alternate'
+              addCell(inner, classString, value)
+         }
+          result = `<tr>${inner}</tr>`
+       //  return result 
+         
+     }
+ }  
 
-        result = `<tr>${inner}</tr>`
-    }
-}
-
-// Only edit above
 
 const current = new Date()
 document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`
 
-const data = createData()
-document.querySelector('[data-content]').innerHTML = createHtml(data)
+const data = createHtml()
+document.querySelector('[data-content]').innerHTML = data   //createHtml(data)   
+
+/* const who = document.querySelector('[data-title]');
+const who2 = document.querySelector('[data-content]')
+
+let light =["neo"+ "cozy" , "Jesus"]
+light.whom = 2
+
+for (let names of light ){
+    who.innerHTML = light;
+    who2.innerHTML = light 
+} */
+
+
+// TRY TO RETURN THE ADDCELL FUNCTION IN THE OTHER FUNCTION
