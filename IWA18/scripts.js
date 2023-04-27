9fe9953697587dea9fab35361cc8e65d8cd2a8b8
NeoDitjoe <ditjoeneo03@gmail.com>
2023-04-27 21:37:03 +0200

    h

--- IWA18/scripts.js
+++ IWA18/scripts.js
@@ -8,7 +8,7 @@ import { createOrderData, updateDragging} from './data.js'
  * until an element with a `data-area` attribute is found. Once found both the
  * active dragging column is set in the `state` object in "data.js" and the HTML
  * is updated to reflect the new column.
- *
+ *              
  * @param {Event} event 
  */
 const handleDragOver = (event) => {
@@ -28,23 +28,33 @@ const handleDragOver = (event) => {
     updateDragging({ over: column })
     updateDraggingHtml({ over: column })
 }
-// fetching help information
 
 
 // actions for when button is clicked
 const handleDragStart = (event) => {}
 const handleDragEnd = (event) => {}
-function handleHelpToggle(){
+const handleHelpToggle = (event)  => {
     html.help.overlay.style.display = "block"
 }
-const handleAddToggle = (event) => {}
+function handleHelpCancel(){
+    html.help.overlay.style.display = "none"
+    html.other.add.focus()    
+}
+const handleAddToggle = () => {
+    html.add.overlay.style.display = 'block'
+    
+}
+const handleAddCancel = () => {
+    html.add.overlay.style.display = 'none'
+    html.other.add.focus() 
+}
 const handleAddSubmit = (event) => {}
 const handleEditToggle = (event) => {}
 const handleEditSubmit = (event) => {}
 const handleDelete = (event) => {}
 
-html.add.cancel.addEventListener('click', handleAddToggle)
-html.other.add.addEventListener('click', handleAddToggle)
+html.add.cancel.addEventListener('click', handleAddCancel) //used
+html.other.add.addEventListener('click', handleAddToggle) //used
 html.add.form.addEventListener('submit', handleAddSubmit)
 
 html.other.grid.addEventListener('click', handleEditToggle)
@@ -52,8 +62,8 @@ html.edit.cancel.addEventListener('click', handleEditToggle)
 html.edit.form.addEventListener('submit', handleEditSubmit)
 html.edit.delete.addEventListener('click', handleDelete)
 
-html.help.cancel.addEventListener('click', handleHelpToggle)
-html.other.help.addEventListener('click', handleHelpToggle) // using
+html.help.cancel.addEventListener('click', handleHelpCancel) //used
+html.other.help.addEventListener('click', handleHelpToggle) // used
 
 for (const htmlColumn of Object.values(html.columns)) {
     htmlColumn.addEventListener('dragstart', handleDragStart)
@@ -63,3 +73,6 @@ for (const htmlColumn of Object.values(html.columns)) {
 for (const htmlArea of Object.values(html.area)) {
     htmlArea.addEventListener('dragover', handleDragOver)
 }
+
+
+
